620e27123d67a47c997f0d75d6e84921ce05e163
akunito <diego88aku@gmail.com>
2022-11-24 18:06:03 +0100

    board working properly. cells change style when mouse is over them

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
+    let cell = document.createElement('div');
     cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = 'grid-item';
   };
 };
 
-makeRows(16, 16);
+makeRows(32, 32);
 
+const cells = document.querySelectorAll('.grid-item');
+
+cells.forEach(cell => {
+  cell.addEventListener('mouseover', () => {
+    cell.className = 'permahover'
+    //cell.style.cssText = "background-color: purple;"
+  })
+});
