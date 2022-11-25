cc2a822b8dc7c07057bbb9b1a6e385d1715cad97
akunito <diego88aku@gmail.com>
2022-11-25 16:06:44 +0100

    reset button implemented - still code is to be checked

--- script.js
+++ script.js
@@ -6,29 +6,67 @@ function makeRows(rows, cols) {
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement('div');
     cell.innerText = (c + 1);
-    container.appendChild(cell).className = 'grid-item';
+    container.appendChild(cell).className = 'cell grid-item';
   };
 };
+
 
 const vrows = prompt("Insert how many Vertical cells (32 recommended)")
 const hrows = prompt("Insert how many Vertical cells (32 recommended)")
 
 makeRows(vrows, hrows);
 
+
+// setting cells that have been hover
 const cells = document.querySelectorAll('.grid-item');
 
 cells.forEach(cell => {
   cell.addEventListener('mouseover', () => {
-    cell.className = 'permahover'
+    cell.className = 'cell permahover'
     //cell.style.cssText = "background-color: purple;"
   })
 });
 
-const reset = document.querySelector('button');
+// clear button
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => {
+  cells.forEach(cell => {
+      cell.className = 'cell grid-item'
+    })
+});
 
 // reset button
+const reset = document.querySelector('.reset');
+
 reset.addEventListener('click', () => {
+  const toRemove = document.querySelectorAll('.cell')
+
+  toRemove.forEach(cell => {
+    container.removeChild(cell)
+  })
+  
+  const vrows = prompt("Insert how many Vertical cells (32 recommended)")
+  const hrows = prompt("Insert how many Vertical cells (32 recommended)")
+
+  makeRows(vrows, hrows);
+
+  // setting cells that have been hover
+  const cells = document.querySelectorAll('.grid-item');
+
   cells.forEach(cell => {
-      cell.className = 'grid-item'
+    cell.addEventListener('mouseover', () => {
+      cell.className = 'cell permahover'
+      //cell.style.cssText = "background-color: purple;"
     })
+  });
+
+  // clear button
+  const clear = document.querySelector('.clear');
+
+  clear.addEventListener('click', () => {
+    cells.forEach(cell => {
+        cell.className = 'cell grid-item'
+      })
+  });
 });
